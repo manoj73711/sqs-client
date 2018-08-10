bbeedc8f498ebe0c423436768faeafd6613e38ef
Manoj Lekkalapudi <Manoj.Lekkalapudi.consultant@nielsen.com>
2018-08-09 20:18:54 -0400

    adding all the working files to github

---
+++ src/main/java/com/syntel/sqs/App.java
@@ -0,0 +1,13 @@
+package com.syntel.sqs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
